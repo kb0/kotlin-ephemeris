7493e4309345f9e4aa6f6b5a6a492d23267c0a4a
Kirill Bychkov <bychkov.kirill.0@gmail.com>
2023-01-06 20:16:03 +0300

    add tests

--- src/test/kotlin/com/kbapps/ephemeris/planet/CalculatorTest.kt
+++ src/test/kotlin/com/kbapps/ephemeris/planet/CalculatorTest.kt
@@ -4,10 +4,11 @@ import com.kbapps.ephemeris.Constants
 import com.kbapps.ephemeris.JulianDate
 import com.kbapps.ephemeris.Utils
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
-import java.time.LocalTime
-import java.time.ZonedDateTime
+import java.time.*
+import java.time.format.DateTimeFormatter
 
 
 class CalculatorTest {
@@ -96,7 +97,8 @@ class CalculatorTest {
     ) {
         val grDate = ZonedDateTime.parse(date)
 
-        val jdTimeStart = JulianDate.toJD(grDate.withHour(0).withMinute(0).withSecond(0).withNano(1))
+        val jdTimeStart =
+            JulianDate.toJD(grDate.withHour(0).withMinute(0).withSecond(0).withNano(1))
         val planetDayPosition =
             model.calculatePlanetPosition(
                 jdTimeStart[0],
@@ -107,10 +109,16 @@ class CalculatorTest {
 
         val jdTime = JulianDate.toJD(grDate)
         val planetPosition =
-            model.calculatePlanetPosition(jdTime[0], JulianDate.tFromJD(jdTime[0], jdTime[1]), latitude, longitude)
+            model.calculatePlanetPosition(
+                jdTime[0],
+                JulianDate.tFromJD(jdTime[0], jdTime[1]),
+                latitude,
+                longitude
+            )
 
         // calculate transit time
-        val transitPosition = Calculator.calculateEventPosition(jdTime[1], model, planetDayPosition, 0.0)
+        val transitPosition =
+            Calculator.calculateEventPosition(jdTime[1], model, planetDayPosition, 0.0)
         if (transitTime != null) {
             assertNotNull(transitPosition)
             assertEquals(
@@ -118,7 +126,12 @@ class CalculatorTest {
                 Utils.toLocalTime(transitPosition?.jd, grDate.offset),
                 "incorrect transit time"
             )
-            assertEquals(transitEl ?: 0.0, transitPosition!!.elevationInDeg, 0.01, "incorrect transit elevation")
+            assertEquals(
+                transitEl ?: 0.0,
+                transitPosition!!.elevationInDeg,
+                0.01,
+                "incorrect transit elevation"
+            )
         } else {
             assertNull(transitPosition)
         }
@@ -160,5 +173,67 @@ class CalculatorTest {
         } else {
             assertNull(dropPosition)
         }
+    }
+
+
+    @Test
+    fun testEquinox() {
+        val latitude = 55.0;
+        val longitude = 37.0;
+
+        IntRange(0, 365 * 12).forEach {
+            val grDate = ZonedDateTime.of(2022, 3, 20, 0, 0, 0, 1, ZoneId.systemDefault())
+                .plusHours(4 * it.toLong())
+
+            val jdTimeStart =
+                JulianDate.toJD(grDate.withHour(0).withMinute(0).withSecond(0).withNano(1))
+
+            val dat = Solar().calculatePlanetPosition(
+                jdTimeStart[0],
+                JulianDate.tFromJD(jdTimeStart[0], jdTimeStart[1]),
+                latitude,
+                longitude
+            )
+
+            println(
+                arrayOf(
+                    grDate.toLocalDateTime(),
+                    dat.jd,
+                    dat.rightAscension,
+                    dat.rightAscension * 180.0 / Math.PI
+                )
+                    .joinToString(";")
+                    .replace(".", ",")
+            )
+        }
+    }
+
+    @Test
+    fun testYearMilestone() {
+        this.javaClass.classLoader.getResourceAsStream("solar_equinox_solstice.txt")
+            ?.bufferedReader()
+            ?.lines()
+            ?.skip(1)
+            ?.filter {
+                it.length > 10
+            }
+            ?.forEach {
+                val year = it.substring(1, 5).trim()
+
+                mapOf(
+                    "EQUINOX_SPRING" to it.substring(11, 24),
+                    "EQUINOX_AUTUMN" to it.substring(47, 60),
+                    "SOLSTICE_SUMMER" to it.substring(29, 42),
+                    "SOLSTICE_WINTER" to it.substring(65, 78)
+                ).forEach { (name, str) ->
+                    val date = LocalDateTime.parse(
+                        year + " " + str.trim(),
+                        DateTimeFormatter.ofPattern("yyyy MMM dd  HH:mm"))
+
+                    println("ts(${date.atZone(ZoneOffset.UTC).toEpochSecond()}) to $name,")
+                }
+
+
+            }
     }
 }
